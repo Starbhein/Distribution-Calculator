0b9ad8329746ee25f8968f5ec06eff1a26a24987
Starbhein <starbheingt01@gmail.com>
2026-05-22 16:31:39 -0600

    im losing my mind

---
+++ internal/core/distributions/normal.go
@@ -0,0 +1,44 @@
+package distributions
+
+import (
+	"errors"
+	"math"
+)
+
+type Normal struct {
+	Average            float64
+	StdandardDeviation float64
+}
+
+func NewNormal(Average, StandardDeviation float64) (*Normal, error) {
+	if StandardDeviation <= 0 {
+		return nil, errors.New("the standard deviation parameter must be a positive number")
+	}
+
+	return &Normal{Average: Average, StdandardDeviation: StandardDeviation}, nil
+}
+
+func (n Normal) Avg() float64 {
+	return n.Average
+}
+
+func (n Normal) StdDev() float64 {
+	return n.StdandardDeviation
+}
+
+func (n Normal) Variance() float64 {
+	return n.StdandardDeviation * n.StdandardDeviation
+}
+
+func (n Normal) PDF(x float64) (float64, error) {
+	z := (x - n.Average) / n.StdandardDeviation
+	exponent := -.5 * z * z
+	coefficient := 1 / (n.StdandardDeviation * math.Sqrt(2*math.Pi))
+
+	return coefficient * math.Exp(exponent), nil
+}
+
+func (n Normal) CDF(x float64) (float64, error) {
+	f := 0.5 * (1 + math.Erf((x-n.Average)/(n.StdandardDeviation*math.Sqrt(2))))
+	return f, nil
+}
